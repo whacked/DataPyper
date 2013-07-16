430d6d219b7ff9971cbe3b4ca5b25adf33146769
natto <natto@natto-tp.none>
2013-07-16 19:30:18 -0400

    add setuptools setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="DataPyper",
+        version = "0.0.1",
+        description = "nipype interface for csv and pandas data structures",
+        packages = ["DataPyper",],
+        )
